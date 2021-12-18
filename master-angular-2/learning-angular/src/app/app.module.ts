95e5662da562d4285aac81e3fd0d6d718a80fc73
Quintin Henn <quintin.henn@gmail.com>
2021-12-18 10:41:51 +0100

    feat: master-angular-2 components

--- master-angular-2/learning-angular/src/app/app.module.ts
+++ master-angular-2/learning-angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageComponent } from './image/image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageComponent
   ],
   imports: [
     BrowserModule,
